b4d809616d86de259b16ee80d9e8d86113804e0f
abialbon <dr.abialbon@gmail.com>
2019-11-19 10:08:17 +0000

    added tests and cleaned code

--- c++/addneg/main.cpp
+++ c++/addneg/main.cpp
@@ -16,27 +16,19 @@ int sub(int x, int y) {
 }
 
 int mul(int x, int y) {
+    int a = y < 0 ? neg(y) : y;
     int accum = 0;
-    if (y > 0) {
-        for (int i = 0; i < y; ++i) {
-            accum = add(accum, x);
-        }
-    } else {
-        for (int i = y; i < 0; ++i) {
-            accum = add(accum, x);
-        }
-        return neg(accum);
+    for (int i = 0; i < a; ++i) {
+        accum = add(accum, x);
     }
+    if (y < 0) return neg(accum);
     return accum;
 }
 
 int d(int x, int y) {
     if (y == 0) throw "Division by zero!";
-    int a = x;
-    if (x < 0) a = neg(x);
-    int c = y;
-    if (y < 0) c = neg(y);
-
+    int a = x < 0 ? neg(x) : x;
+    int c = y < 0 ? neg(y) : y;
     int count = 0;
     int accum = 0;
     while(add(accum, c) <= a) {
@@ -49,6 +41,19 @@ int d(int x, int y) {
 }
 
 int main() {
-    cout << d(-144, -144) << endl;
+    for (int i = 0; i < 1000; ++i)
+    {
+        int r1 = rand() % 1000;
+        int r2 = rand() % 1000;
+        if (rand() % 100 < 50) r1 = -r1;
+        if (rand() % 100 < 50) r2 = -r2;
+        if (sub(r1, r2) != r1 - r2)
+            cout << "Subtraction did not work for " << r1 << " " << r2 << endl;
+        if (mul(r1, r2) != r1 * r2)
+            cout << "Multiplication did not work for " << r1 << " " << r2 << endl;
+        if (d(r1, r2) != r1 / r2)
+            cout << "Division did not work for " << r1 << " " << r2 << endl;
+    }
+    cout << "Successfully completed!" << endl;
     return 0;
 }
